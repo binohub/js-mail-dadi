318c7cd2f5d73c96b953d73107d180683c81c4c3
binohub <yeebino001@gmail.com>
2022-06-17 16:19:59 +0200

    dadi - done

--- js/dadi.js
+++ js/dadi.js
@@ -1,6 +1,18 @@
-//! genera il userRandomNumber = Math.floor(Math.random() * 6) + 1
-//! genera il botRandomNumber = Math.floor(Math.random() * 6) + 1
 
-//! se userRandomNumber > botRandomNumber
-    //! stampa: hai vinto
-    //! altrimenti ha perso
+//! genera il userRandomNumber
+const userRandomNumber = Math.floor(Math.random() * 6) + 1;
+console.log(`il tuo numero ${userRandomNumber}`);
+//! genera il botRandomNumber
+const botRandomNumber = Math.floor(Math.random() * 6) + 1;
+console.log(`il bot ha scelto ${botRandomNumber}`);
+
+// ! if uguale pareggio
+if (botRandomNumber == userRandomNumber) {
+    console.log("pareggio");
+// ! if minore vinto
+}else if(botRandomNumber < userRandomNumber) {
+    console.log("hai vinto!");
+// ! sennò hai perso
+}else{
+    console.log("hai perso!");
+}
