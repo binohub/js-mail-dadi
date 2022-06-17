9376eacbda3c674f2026af454e91b4ac18f1a10d
binohub <yeebino001@gmail.com>
2022-06-17 15:58:42 +0200

    mail - finish

--- js/mail.js
+++ js/mail.js
@@ -1,12 +1,31 @@
-
-
 //! lista di chi può accedere
-//! mailsApproved
+const mails = ["js@gmail.com", "for@gmail.com",
+"array@gmail.com", "while@gmail.com", "const@gmail.com",
+"let@gmail.com", "console.log@gmail.com"];
+
+//! mail approvata? 
+let mailsApproved = false;
+
 //! chiedi all'utente la sua mail
+const mail = prompt("digita la tua mail");
 
-//! controlla la lista di chi può accedere (ciclo)
-    //! se presente in lista restituisci ad mailsApproved true
+// !count
+const count = mails
 
+//! controlla se presente in lista mediante un ciclo
+for (let index = 0; index < count.length; index++) {
+    element = mails[index];
+    // console.log(index + element);
+    if (mails[index] === mail) {
+        console.log(`Index: ${index}  `+ element);
+
+        //! se presente in lista restituisci ad mailsApproved true
+        mailsApproved = true;
+    }
+}
 //! se mailsApproved true
-    //! stampa "mail riconosciuta, puoi acccedere"
-    //! altrimenti stampa "ritenta, mail errata"
+if (mailsApproved === true){
+    console.log(`mail riconosciuta, puoi accedere`);   
+}else{
+    console.log(`ritenta, mail errata o non esistente`);
+}
