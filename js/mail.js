7323c1e3a69a2a33040cb42f6406eb2710c1c602
binohub <yeebino001@gmail.com>
2022-06-17 15:18:04 +0200

    mail - start

---
+++ js/mail.js
@@ -0,0 +1,12 @@
+
+
+//! lista di chi può accedere
+//! mailsApproved
+//! chiedi all'utente la sua mail
+
+//! controlla la lista di chi può accedere (ciclo)
+    //! se presente in lista restituisci ad mailsApproved true
+
+//! se mailsApproved true
+    //! stampa "mail riconosciuta, puoi acccedere"
+    //! altrimenti stampa "ritenta, mail errata"
